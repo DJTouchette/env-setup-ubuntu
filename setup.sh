71f4d7fefb5ef81c352cf78a89cf92e2cee5a10d
Damien Touchette <damien@cultivatedcode.com>
2021-06-11 15:06:52 -0600

    update

--- setup.sh
+++ setup.sh
@@ -7,8 +7,9 @@ cp .zshrc ~/
 
 # NeoVim setup
 source ./setup/neovim.sh
-rm ~/config/neovim/init.vim
-cp ./init.vim ~/config/neovim
+mkdir -p ~/config/neovim
+rm -f ~/config/neovim/init.vim
+cp ./init.vim ~/.config/neovim
 
 # Docker completion
 source ./setup/docker-completion.sh
